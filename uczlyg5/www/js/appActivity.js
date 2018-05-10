bded41c59bb6e5ac4d98114aeb98cb80440a1c75
Claire Ellul <c.ellul@ucl.ac.uk>
2018-05-10 19:42:23 -0400

    show an alert,chanig

--- uczlyg5/www/js/appActivity.js
+++ uczlyg5/www/js/appActivity.js
@@ -1,4 +1,8 @@
-// show a simple pop-up alert
+﻿// show a simple pop-up alert
 function menuClicked() {
 alert("You clicked the menu");
+}
+// replace graphic
+function replaceGraphs() {
+document.getElementById("graphdiv").innerHTML ="<img src=' images/ucl.png'>"
 }
